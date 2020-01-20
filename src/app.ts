be666c2a686d71dc357de94c1f4329f90a7486e3
valorad <valorad@outlook.com>
2020-01-19 21:40:33 -0500

    feat: set path and port dynamically.

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,36 @@ import * as Koa from "koa";
 import * as logger from "koa-logger";
 import * as cors from "@koa/cors";
 import * as serve from 'koa-static';
+import { parse } from "yargs";
 
 class App {
   app = new Koa();
   port = 3000;
+  argv = parse();
+  clientPath = "";
 
   constructor() {
+
+    if ( !isNaN(this.argv.port as number) ) {
+      this.port = this.argv.port as number;
+    }
+
+    let pathToServe = this.argv._[0];
+    if (!pathToServe) {
+      pathToServe = ".";
+    }
+    // make path absolute
+    this.clientPath = resolve(pathToServe);
+    
     this.app.use(logger());
     this.app.use(cors());
-    this.app.use(serve(resolve(__dirname, ".")));
-    
+    this.app.use(serve(this.clientPath));
+
     this.app.listen(this.port, () => {
-      console.log(`🎧 Mini Server is listening on port ${this.port}`);
+      console.log(`🎧 Mini Server is listening on port: ${this.port}`);
+      console.log(`📺 Mini Server is currently serving: ${this.clientPath}`);
     });
+
   }
 }
 
